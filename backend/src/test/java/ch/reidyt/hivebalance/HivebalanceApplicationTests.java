4318d1134491609878eed749fd81e01e3f5ce43c
Thibault Reidy <147397675+ReidyT@users.noreply.github.com>
2025-01-06 16:26:23 +0100

    feat: initialise Spring backend (#1)

---
+++ backend/src/test/java/ch/reidyt/hivebalance/HivebalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.reidyt.hivebalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HivebalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
