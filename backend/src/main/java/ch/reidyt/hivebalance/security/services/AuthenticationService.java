0ed0045a218ed38052fa14e74ad5c721f8ec7e5c
Thibault Reidy <thibault.reidy@hotmail.ch>
2025-02-28 11:45:26 +0100

    fix(AuthenticationService): throw user conflicts exception if username or email already exist

--- backend/src/main/java/ch/reidyt/hivebalance/security/services/AuthenticationService.java
+++ backend/src/main/java/ch/reidyt/hivebalance/security/services/AuthenticationService.java
@@ -1,6 +1,8 @@
 package ch.reidyt.hivebalance.security.services;
 
 import ch.reidyt.hivebalance.security.dtos.UserRegistrationDTO;
+import ch.reidyt.hivebalance.security.errors.EmailConflictException;
+import ch.reidyt.hivebalance.security.errors.UsernameConflictException;
 import ch.reidyt.hivebalance.user.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -24,6 +26,16 @@ public class AuthenticationService {
 
     @Transactional
     public Authentication registerUser(UserRegistrationDTO userRegistrationDTO) {
+        var res = userRepository.findByEmailOrUsername(userRegistrationDTO.email(), userRegistrationDTO.username());
+        var emailAlreadyExist = res.stream().anyMatch((r) -> r.email().equalsIgnoreCase(userRegistrationDTO.email()));
+        var usernameAlreadyExist = res.stream().anyMatch((r) -> r.username().equalsIgnoreCase(userRegistrationDTO.username()));
+
+        if (emailAlreadyExist) {
+            throw new EmailConflictException(userRegistrationDTO.email());
+        } else if (usernameAlreadyExist) {
+            throw new UsernameConflictException(userRegistrationDTO.username());
+        }
+
         userRepository.save(userRegistrationDTO.toUser(passwordEncoder));
 
         var usernamePasswordToken = UsernamePasswordAuthenticationToken.unauthenticated(userRegistrationDTO.email(),
